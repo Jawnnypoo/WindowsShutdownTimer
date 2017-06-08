01d7866794a6a065f44dda4ebfd5a6d1e15262d4
John Carlson <jawnnypoo@gmail.com>
2017-06-08 17:19:32 -0500

    Some cleanup

--- lib/windows-shutdown-timer.rb
+++ lib/windows-shutdown-timer.rb
@@ -46,13 +46,13 @@ module WindowsShutdownTimer
     end
 
     def unzip(file, destination)
-      Zip::File.open(file) {|zip_file|
-        zip_file.each {|f|
-          f_path=File.join(destination, f.name)
+      Zip::File.open(file) do |zip_file|
+        zip_file.each do |f|
+          f_path = File.join(destination, f.name)
           FileUtils.mkdir_p(File.dirname(f_path))
           zip_file.extract(f, f_path) unless File.exist?(f_path)
-        }
-      }
+        end
+      end
     end
 
   end
